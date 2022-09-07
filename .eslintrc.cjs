b506450b2e10acdb07a2af02f46f21dc7d3d3fe7
AlesyaD <alesya.dubik.99@mail.ru>
2022-09-07 18:41:13 +0300

    project configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-no-target-blank': 0,
+    'react/button-has-type': 0,
+  },
+};
